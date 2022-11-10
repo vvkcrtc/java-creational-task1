577f7fa194663fe3736e3a26f8f85a930b008efb
vvkcrtc <105951181+vvkcrtc@users.noreply.github.com>
2022-11-10 22:32:47 +0300

    Delete PersonBuilder.java

--- PersonBuilder.java
+++
@@ -1,8 +0,0 @@
-public interface PersonBuilder {
-    public PersonBuilder setName(String name);
-    public PersonBuilder setSurname(String surname);
-    public PersonBuilder setAge(int age);
-    public PersonBuilder setAddress(String address);
-
-    public Person build();
-}
